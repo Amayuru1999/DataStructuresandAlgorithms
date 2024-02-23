ee4fee554e545cd10893702c683546aa85ab583a
Amayuru1999 <amarasingheau@gmail.com>
2024-02-23 12:31:40 +0530

    First commit

---
+++ src/MultiThreading/Test.java
@@ -0,0 +1,33 @@
+package MultiThreading;
+
+class A extends Thread{
+    public void run(){
+        for(int i=0;i<10;i++){
+            System.out.println("Hello");
+            try{
+                Thread.sleep(500);
+            }catch (Exception e){}
+        }
+    }
+}
+
+class B extends Thread{
+    @Override
+    public void run() {
+        for(int i=0;i<10;i++){
+            System.out.println("Welcome");
+            try{
+                Thread.sleep(500);
+            }catch(Exception e){}
+        }
+    }
+}
+
+public class Test{
+    public static void main(String[] args) {
+        A obj1 =new A();
+        obj1.start();
+        B obj2=new B();
+        obj2.start();
+    }
+}
